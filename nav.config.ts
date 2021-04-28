5673bea877a51f60abd13aa9cd9ebd45ec6123b7
linyuxuanlin <me@wiki-power.com>
2021-04-28 15:17:47 +0800

    up

--- nav.config.ts
+++ nav.config.ts
@@ -2,16 +2,16 @@ import { IConfig } from './src/types'
 
 const c: IConfig = {
   // [必填], 请填写您的仓库地址
-  gitRepoUrl: 'https://github.com/xjh22222228/nav',
+  gitRepoUrl: 'https://github.com/linyuxuanlin/Wiki-nav',
 
   // 部署分支
   branch: 'main',
 
   // 路由是否Hash模式, 如果是部署在github pages 务必设为 true
-  hashMode: true,
+  hashMode: false,
 
   // 是否显示右上角的 Github 图标
-  showGithub: true,
+  showGithub: false,
 
   // 您的网站地址，有利于SEO
   homeUrl: 'https://nav3.cn',
